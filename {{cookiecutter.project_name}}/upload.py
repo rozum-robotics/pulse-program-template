a900460969c45f29c1735dce3c999d0e2d2b6780
Nikolay Lagutko <mikalai.lagutka@rozum.com>
2020-03-12 18:46:57 +0300

    upload script

---
+++ {{cookiecutter.project_name}}/upload.py
@@ -0,0 +1,64 @@
+import getopt
+import os
+import sys
+
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
+from zipfile import ZipFile
+
+SANDBOX_USERNAME = 'sandbox'
+SANDBOX_PASSWORD = 'sandbox2'
+SANDBOX_PORT = 22
+
+SCRIPT = os.path.realpath(__file__)
+SCRIPT_ROOT = os.path.dirname(SCRIPT)
+
+
+def zip_all():
+    file_exclusions = [
+        '{{cookiecutter.project_name}}.zip',
+        '.gitignore',
+        'development.txt'
+    ]
+    dir_exclusions = [
+        'venv',
+        'test'
+    ]
+
+    with ZipFile('{{cookiecutter.project_name}}.zip', 'w') as zip_file:
+        for root, dirs, files in os.walk(SCRIPT_ROOT, topdown=True):
+            dirs[:] = [d for d in dirs if d not in dir_exclusions]
+
+            rel_root = os.path.relpath(root, SCRIPT_ROOT)
+
+            for file in files:
+                if file not in file_exclusions:
+                    zip_file.write(os.path.join(rel_root, file))
+
+
+def upload(host, zip_path):
+    ssh = SSHClient()
+    ssh.set_missing_host_key_policy(AutoAddPolicy())
+    ssh.connect(hostname=host, username=SANDBOX_USERNAME, password=SANDBOX_PASSWORD, port=SANDBOX_PORT)
+    ssh.exec_command('mkdir -p /home/sandbox/player')
+
+    scp = SCPClient(ssh.get_transport())
+    scp.put(os.path.join(SCRIPT_ROOT, zip_path), '/home/sandbox/player')
+
+    ssh.exec_command(f'unzip /home/sandbox/player/{zip_path} -d /home/sandbox/player/{{cookiecutter.project_name}}')
+    ssh.exec_command('/home/sandbox/player/{{cookiecutter.project_name}}/init.sh')
+
+    scp.close()
+    ssh.close()
+
+
+def main(argv):
+    opts, args = getopt.getopt(argv, '')
+
+    zip_all()
+
+    upload(args[0], '{{cookiecutter.project_name}}.zip')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
