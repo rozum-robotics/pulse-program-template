f8ece67c0d16e1760f224500225f7e5863b1fe22
Ilya Grozov <ilya.grozov@rozum.com>
2020-04-02 11:26:10 +0300

    [*] small refactoring of upload script

--- {{cookiecutter.project_name}}/upload.py
+++ {{cookiecutter.project_name}}/upload.py
@@ -1,13 +1,15 @@
 import getopt
 import os
 import sys
+import argparse
+import getpass
 
 from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
 from zipfile import ZipFile
 
-SANDBOX_USERNAME = 'sandbox'
-SANDBOX_PASSWORD = 'sandbox2'
+SANDBOX_USERNAME = "sandbox"
+SANDBOX_PASSWORD = "sandbox2"
 SANDBOX_PORT = 22
 
 SCRIPT = os.path.realpath(__file__)
@@ -16,19 +18,16 @@ SCRIPT_ROOT = os.path.dirname(SCRIPT)
 
 def zip_all():
     file_exclusions = [
-        '{{cookiecutter.project_name}}.zip',
-        '.gitignore',
-        'development.txt'
+        "{{cookiecutter.project_name}}.zip",
+        ".gitignore",
+        "development.txt",
     ]
-    dir_exclusions = [
-        'venv',
-        'test'
-    ]
+    dir_exclusions = ["venv", "test"]
 
-    with ZipFile('{{cookiecutter.project_name}}.zip', 'w') as zip_file:
+    with ZipFile("{{cookiecutter.project_name}}.zip", "w") as zip_file:
         for root, dirs, files in os.walk(SCRIPT_ROOT, topdown=True):
             dirs[:] = [d for d in dirs if d not in dir_exclusions]
-
+            
             rel_root = os.path.relpath(root, SCRIPT_ROOT)
 
             for f in files:
@@ -39,26 +38,39 @@ def zip_all():
 def upload(host, zip_path):
     ssh = SSHClient()
     ssh.set_missing_host_key_policy(AutoAddPolicy())
-    ssh.connect(hostname=host, username=SANDBOX_USERNAME, password=SANDBOX_PASSWORD, port=SANDBOX_PORT)
-    ssh.exec_command('mkdir -p /home/sandbox/player')
+    pwd = getpass.getpass("Enter password: ")
+    ssh.connect(
+        hostname=host,
+        username=SANDBOX_USERNAME,
+        password=pwd,
+        port=SANDBOX_PORT,
+    )
+    ssh.exec_command("mkdir -p /home/sandbox/player")
 
     scp = SCPClient(ssh.get_transport())
-    scp.put(os.path.join(SCRIPT_ROOT, zip_path), '/home/sandbox/player')
+    scp.put(os.path.join(SCRIPT_ROOT, zip_path), "/home/sandbox/player")
 
-    ssh.exec_command(f'unzip /home/sandbox/player/{zip_path} -d /home/sandbox/player/{{cookiecutter.project_name}}')
-    ssh.exec_command('/home/sandbox/player/{{cookiecutter.project_name}}/init.sh')
+    ssh.exec_command(
+        f"unzip /home/sandbox/player/{zip_path} -d /home/sandbox/player/{{cookiecutter.project_name}}"
+    )
+    ssh.exec_command(
+        "/home/sandbox/player/{{cookiecutter.project_name}}/init.sh"
+    )
 
     scp.close()
     ssh.close()
 
 
-def main(argv):
-    opts, args = getopt.getopt(argv, '')
-
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host")
+    args = parser.parse_args()
+    robot_host = args.host
+    print(f"Uploading project to {robot_host}")
     zip_all()
 
-    upload(args[0], '{{cookiecutter.project_name}}.zip')
+    upload(robot_host, "{{cookiecutter.project_name}}.zip")
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
