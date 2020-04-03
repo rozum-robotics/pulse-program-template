1e7597ea0be0d7c0ee7646241f2aac3ca044dc45
Ilya Grozov <ilya.grozov@rozum.com>
2020-04-03 17:50:12 +0300

    [*] program template

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/program.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/program.py
@@ -1,1 +1,28 @@
+from pulseapi import RobotPulse
 
+
+robot = RobotPulse("localhost:8081")
+
+
+def before_all():
+    pass
+
+
+def before_each():
+    pass
+
+
+def execute():
+    pass
+
+
+def after_each():
+    pass
+
+
+def after_all():
+    pass
+
+
+def on_error(exc):
+    pass
