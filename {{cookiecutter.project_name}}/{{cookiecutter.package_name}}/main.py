b1f13b6a289ea4965fc0e20170619306f1857dda
Ilya Grozov <ilya.grozov@rozum.com>
2020-04-03 17:50:27 +0300

    [*] user execution flow

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/main.py
@@ -0,0 +1,27 @@
+import argparse
+
+from {{cookiecutter.package_name}} import program
+
+
+def run(num_iterations):
+    try:
+        program.before_all()
+        for i in range(num_iterations):
+            program.before_each()
+            program.execute()
+            program.after_each()
+        program.after_all()
+    except Exception as exc:
+        program.on_error(exc)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--n_iter", 
+        type=int, 
+        required=True, 
+        description="Number of iterations to perform"
+    )
+    args = parser.parse_args()
+    run(args.n)
