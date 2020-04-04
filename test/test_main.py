39157f71c098eb99c90ed007a871fb46ee637552
Ilya Grozov <ilya.grozov@rozum.com>
2020-04-04 18:16:20 +0300

    [*] program instance class

--- test/test_main.py
+++ test/test_main.py
@@ -7,40 +7,48 @@ def test_main_run(cookies):
     result = cookies.bake()
     sys.path.append(result.project.strpath)
     pulse_program = __import__("pulse_program", fromlist=["main"])
-    pulse_program.main.run(1)
+    pulse_program.main.run(1, "localhost:8081")
 
 
 def test_methods_invocation(cookies, mocker):
     result = cookies.bake()
     sys.path.append(result.project.strpath)
     pulse_program = __import__("pulse_program", fromlist=["main", "program"])
-    program_patch = mocker.patch.object(pulse_program.main, "program")
+    program_patch = mocker.patch.object(pulse_program.main.program, "Instance")
     iterations = 2
-    pulse_program.main.run(iterations)
-    call = mocker.call
+    pulse_program.main.run(iterations, "localhost:8081")
     
-    program_patch.assert_has_calls([
-        call.before_all(),
+    instance = program_patch.return_value
+    call = mocker.call
+    call_enter = call.__enter__()
+    
+    instance.assert_has_calls([
+        call_enter.before_all(),
         
-        call.before_each(),
-        call.execute(),
-        call.after_each(),
+        call_enter.before_each(),
+        call_enter.execute(),
+        call_enter.after_each(),
         
-        call.before_each(),
-        call.execute(),
-        call.after_each(),
+        call_enter.before_each(),
+        call_enter.execute(),
+        call_enter.after_each(),
         
-        call.after_all()
+        call_enter.after_all()
+        
     ])
-    program_patch.on_error.assert_not_called()
+    instance.on_error.assert_not_called()
 
 
 def test_on_error_invocation(cookies, mocker):
     result = cookies.bake()
     sys.path.append(result.project.strpath)
     pulse_program = __import__("pulse_program", fromlist=["main", "program"])
-    program_patch = mocker.patch.object(pulse_program.main, "program")
-    program_patch.execute.side_effect = Exception("Boom!")
-    pulse_program.main.run(1)
-    
-    program_patch.on_error.assert_called_once()
+    program_patch = mocker.patch.object(pulse_program.main.program, "Instance")
+    exception = Exception("Boom!")
+    program_patch.return_value.__enter__.return_value.execute.side_effect = exception
+    try:
+        pulse_program.main.run(1, "localhost:8081")
+    except:
+        instance = program_patch.return_value
+        instance.__exit__.assert_called_once()
+        assert None not in instance.__exit__.call_args
--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/program.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/program.py
@@ -1,28 +1,33 @@
 from pulseapi import RobotPulse
 
 
-robot = RobotPulse("localhost:8081")
 
+class Instance:
+    def __init__(self, robot_ip: str, *args, **kwargs):
+        self._robot_ip = robot_ip
+    
+    def __enter__(self):
+        self.robot = RobotPulse(self._robot_ip)
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_value is not None:
+            self.on_error(exc_value)
+    
+    def before_all(self):
+        print("Before all")
 
-def before_all():
-    print("Before all")
+    def before_each(self):
+        print("Before each")
 
+    def execute(self):
+        print("Execution")
 
-def before_each():
-    print("Before each")
+    def after_each(self):
+        print("After each")
 
+    def after_all(self):
+        print("After all")
 
-def execute():
-    print("Execution")
-
-
-def after_each():
-    print("After each")
-
-
-def after_all():
-    print("After all")
-
-
-def on_error(exc):
-    print("On error")
+    def on_error(self, exc_value):
+        print("On error")
