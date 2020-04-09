e0bbbf64ba9bd4901032a00709eb81bfcb0eb718
Ilya Grozov <ilya.grozov@rozum.com>
2020-04-09 14:43:21 +0300

    [-] unneeded test

--- test/test_bake_context.py
+++ test/test_bake_context.py
@@ -12,15 +12,6 @@ def test_project_name_applied(cookies):
     assert_that(result.project.basename, equal_to(project_name))
 
 
-def test_package_name_applied(cookies):
-    package_name = "package"
-    result = cookies.bake(extra_context={"package_name": package_name})
-    expected_path = pathlib.Path(result.project.strpath) / package_name
-
-    assert_that(expected_path.exists(), is_(True))
-    assert_that(expected_path.is_dir(), is_(True))
-
-
 def test_version_applied(cookies):
     version = "1.15.0"
     result = cookies.bake(extra_context={"version": version})
