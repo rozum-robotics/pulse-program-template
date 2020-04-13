2ceffb06759fc64a43ee4e97f5860277d4ed385e
Ilya Grozov <ilya.grozov@rozum.com>
2020-04-13 15:21:22 +0300

    [*] optional port arg and black formatting

--- {{cookiecutter.project_name}}/upload.py
+++ {{cookiecutter.project_name}}/upload.py
@@ -24,8 +24,7 @@ def wait_cmd(stds):
 
 def make_sdist():
     proc = subprocess.run(
-        ["python3", "setup.py", "sdist", "--formats=zip"],
-        cwd=SCRIPT_ROOT
+        ["python3", "setup.py", "sdist", "--formats=zip"], cwd=SCRIPT_ROOT
     )
     if proc.returncode:
         raise RuntimeError("Failed to make source distribution")
@@ -34,12 +33,12 @@ def make_sdist():
         return "dist/{{cookiecutter.project_name}}-{}.zip".format(version)
 
 
-def upload(host, venv_init=True):
+def upload(host, port, venv_init=True):
     # TODO: replace prints with logging to file and stdout
     print("Making distribution...", end="")
     dist_path = make_sdist()
     print("done {}".format(dist_path))
-    
+
     print("Establishing SSH connection to {}:{}...".format(host, SANDBOX_PORT))
     ssh = SSHClient()
     ssh.set_missing_host_key_policy(AutoAddPolicy())
@@ -51,36 +50,37 @@ def upload(host, venv_init=True):
         port=SANDBOX_PORT,
     )
     print("Connection established")
-    
+
     print("Preparing directory for the project...", end="")
     project_path = "/home/sandbox/player/{{cookiecutter.project_name}}"
     venv_path = "{}/venv".format(project_path)
     ssh.exec_command("mkdir -p {}".format(project_path))
     print("done")
-    
-    if venv_init:    
+
+    if venv_init:
         # intialize venv for project
         print("Intializing virtual environment...", end="")
         wait_cmd(ssh.exec_command("python3 -m venv {}".format(venv_path)))
         print("done {}".format(venv_path))
-    
+
     print("Uploading distribution ...", end="")
     with SCPClient(ssh.get_transport()) as scp:
         scp.put(os.path.join(SCRIPT_ROOT, dist_path), project_path)
     print("done")
-    
+
     print("Installing distribution...", end="")
-    install_cmd = " ".join([
-        "cd {} &&".format(project_path),
-        "venv/bin/python",
-        "-m pip install",
-        os.path.basename(dist_path),
-        "pulse-executor==0.0.1.dev0"
-        "-i https://pip.rozum.com/simple"
-    ])
+    install_cmd = " ".join(
+        [
+            "cd {} &&".format(project_path),
+            "venv/bin/python",
+            "-m pip install",
+            os.path.basename(dist_path),
+            "pulse-executor==0.0.1.dev0" "-i https://pip.rozum.com/simple",
+        ]
+    )
     wait_cmd(ssh.exec_command(install_cmd))
     print("done")
-    
+
     print("Closing SSH connection...", end="")
     ssh.close()
     print("done")
@@ -89,9 +89,12 @@ def upload(host, venv_init=True):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("host")
+    parser.add_argument(
+        "--port", type=int, default=SANDBOX_PORT, required=False
+    )
     args = parser.parse_args()
-    robot_host = args.host
-    upload(robot_host)
+    robot_host, robot_port = args.host, args.port
+    upload(robot_host, robot_port)
 
 
 if __name__ == "__main__":
